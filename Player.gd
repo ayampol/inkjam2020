fa90bdf6f7e743875d521b4bb1a9ce101a621e3b
Alena Yampolskaya <ayampol1@binghamton.edu>
2020-08-02 17:31:00 -0400

    Collisions and stuff

--- Player.gd
+++ Player.gd
@@ -19,7 +19,7 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity*delta)
+	velocity = move_and_slide(velocity)
 
 
 # Called when the node enters the scene tree for the first time.
